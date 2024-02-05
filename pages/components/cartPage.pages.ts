8bfe550ee8c76b4b8edc027f1ad0c25de0227745
Joseph <jkmoore1723@gmail.com>
2024-02-05 16:50:21 -0500

    WIP_ updated Cart Testing

--- pages/components/cartPage.pages.ts
+++ pages/components/cartPage.pages.ts
@@ -3,10 +3,18 @@ import {Page, expect, Locator,} from '@playwright/test'
 export class CartPage {
     page: Page;
     loginOrRegistLink: Locator;
+    mensNavLink: Locator;
+    mensSprayItem: Locator;
+    addToCartBtn: Locator;
+    itemInCart: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.loginOrRegistLink = page.getByRole('link', {name: 'Login or register'});
+        this.mensNavLink = page.getByRole('link', {name: 'Men', exact: true});
+        this.mensSprayItem = page.getByRole('link', {name: 'ck IN2U Eau De Toilette Spray'});
+        this.addToCartBtn = page.getByRole('link', {name: '  Add to Cart '});
+        this.itemInCart = page.getByRole('cell', { name: 'ck IN2U Eau De Toilette Spray' });
 
 
     }
--- tests/cartPage.spec.ts
+++ tests/cartPage.spec.ts
@@ -1,7 +1,7 @@
 import {expect, test} from '@playwright/test'
 import {allure} from 'allure-playwright'
 import { CartPage } from '../pages/components/cartPage.pages'
-import { beforeEach } from 'node:test'
+
 
 test.describe('Cart Page', () => {
   let cartPage: CartPage
@@ -14,15 +14,32 @@ test('Add An Item To The Cart', async ({ page }) => {
     //Navigate To The HomePage
         await allure.step('Navigate To The HomePage',async () => {
         await cartPage.navigateAcct();
-    })
+    });
 
-    //Click On The login Or Register Link
-        await allure.step('Click On The Login Or Register link',async () => {
-        await cartPage.loginOrRegistLink.click();
-    })
+    //Click On The Mens Nav Link
+        await allure.step('Click On The Mens Nav Link',async () => {
+        await cartPage.mensNavLink.click();
+    });
 
-    //
+    //Click On Any Item To View (Test Dat: ck IN2U Eau De Toilette Spray for Him)
+        await allure.step('Click On Any Item To View',async () => {
+        await cartPage.mensSprayItem.click();
+    });
 
+    //Click On The Add To Cart Button
+        await allure.step('Click On The Add To Cart Button',async () => {
+        await cartPage.addToCartBtn.click();
+    });
+
+    //(Assertion) - Verify The Checkout Url
+        await allure.step('Verify The Checkout Url',async () => {
+        await expect(page).toHaveURL('https://automationteststore.com/index.php?rt=checkout/cart');
+    });
+
+    //(Assertion) - Verify Item In Cart Is Present
+        await allure.step('Verify Item In Cart Is Present',async () => {
+        await expect(cartPage.itemInCart).toBeVisible();
+    });
 
   })
   
