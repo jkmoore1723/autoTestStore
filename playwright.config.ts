226dab4f88ad2d9d3ec1168186600255912abb64
Joseph <jkmoore1723@gmail.com>
2024-02-08 17:07:13 -0500

    WIP_Updated Contactpage Tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,24 +11,33 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  
   //Timeouts
   timeout: 30 * 1000,
+  
   //Expected Timeout 
   expect: {
     timeout: 10000
   },
+  
   /* Run tests in files in parallel */
   fullyParallel: true,
+  
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+ 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'],['allure-playwright']],
+  reporter: [['allure-playwright', {outputFolder: 'test-results'}], ['line']],
+ 
   /*Global SetUp*/
   globalSetup: require.resolve('./utils/global-setup'),
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -37,7 +46,7 @@ export default defineConfig({
     storageState: 'loggedInState.json',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    screenshot: 'only-on-failure', trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
--- tests/contactPage.spec.ts
+++ tests/contactPage.spec.ts
@@ -9,53 +9,58 @@ test.describe('Contact Us Page', () => {
     test.beforeEach(async ({ page }) => {
         contactPage = new ContactPage(page);
     })
-     test('Navigate To The Contact Us Page', async ({ page }) => {
-        //Navigate To The Url Site
-            await allure.severity('P1')
-            await allure.step('Navigate To The Url',async () => {
-            await contactPage.navigateCont();
-        })
+test('Navigate To The Contact Us Page', async ({ page }) => {
+    //Navigate To The Url Site
+        await allure.severity('P1')
+        await allure.step('Navigate To The Url',async () => {
+        await contactPage.navigateCont()
+    });
 
-        //(Assertion) - Verify The Url 
-            await allure.step('Verify The Url',async () => {
-            await expect(page).toHaveURL('https://automationteststore.com/index.php?rt=content/contact');
-            })
+    //(Assertion) - Verify The Url 
+        await allure.severity('P1')
+        await allure.step('Verify The Url',async () => {
+        await expect(page).toHaveURL('https://automationteststore.com/index.php?rt=content/contact');
+    });
 
-        //(Assertion) - Verify Address Information & Phone Number Is Present
-            await allure.step('Verify The Address Information & Phone Number',async () => {
-            await expect(contactPage.addressStoreName).toBeVisible();
-            await expect(contactPage.storePhoneNumber).toBeVisible();
-            })
+    //(Assertion) - Verify Address Information & Phone Number Is Present
+        await allure.severity('P1')
+        await allure.step('Verify The Address Information & Phone Number',async () => {
+        await expect(contactPage.addressStoreName).toBeVisible()
+        await expect(contactPage.storePhoneNumber).toBeVisible()
+     });
 
-        });
+});
 
-    test('Fill Out Contact Form', async ({ page }) => {
-        //Navigate To The Url Site
-            await allure.severity('P1')
-            await allure.step('Navigate To The Url',async () => {
-            await contactPage.navigateCont();
-            })
+test('Fill Out Contact Form', async ({ page }) => {
+    //Navigate To The Url Site
+        await allure.severity('P1')
+        await allure.step('Navigate To The Url',async () => {
+        await contactPage.navigateCont()
+    });
         
-        //Fill Out The Contact Form
-            await allure.severity('P1')
-            await allure.step('Fill Out Contact Form',async () => {
-            await contactPage.signUpForm();
-        })
+    //Fill Out The Contact Form
+        await allure.severity('P1')
+        await allure.step('Fill Out Contact Form',async () => {
+        await contactPage.signUpForm()
+    });
 
-        //Click On The Submit Button
-            await allure.step('Click On The Submit Button',async () => {
-            await contactPage.submitButton.click();
-            })
+    //Click On The Submit Button
+        await allure.severity('P1')
+        await allure.step('Click On The Submit Button',async () => {
+        await contactPage.submitButton.click()
+    });
 
-        //(Assertion) - Verify The Contact US Heading
-            await allure.step('Verify The Contact Us Heading',async () => {
-            await expect(contactPage.contactUSHead).toBeVisible();
-            })
+    //(Assertion) - Verify The Contact US Heading
+        await allure.severity('P1')
+        await allure.step('Verify The Contact Us Heading',async () => {
+        await expect(contactPage.contactUSHead).toBeVisible();
+    });
 
-        //(Assertion) - Verify The Successful Message Being Sent
-            await allure.step('Verify The Successful Message Sent',async () => {
-            await expect(contactPage.successfullMessage).toBeVisible();
-            })
+    //(Assertion) - Verify The Successful Message Being Sent
+        await allure.severity('P1')
+        await allure.step('Verify The Successful Message Sent',async () => {
+        await expect(contactPage.successfullMessage).toBeVisible();
+    });
 
-        });  
+    });  
 });
